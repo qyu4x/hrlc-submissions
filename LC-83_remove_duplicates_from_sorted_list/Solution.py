724abb8c31271d419472bc76559e58d80f401c11
Ariq Khoiri <qq.khoiri@gmail.com>
2024-02-24 23:02:23 +0700

    38ms Beats75.62%of users

--- LC-83_remove_duplicates_from_sorted_list/Solution.py
+++ LC-83_remove_duplicates_from_sorted_list/Solution.py
@@ -8,31 +8,50 @@ class ListNode:
         self.next = next
 class Solution:
 
-    def getSize(self, head: Optional[ListNode]) -> int:
+    def print(self, head: Optional[ListNode]) -> None:
         current = head
-        count = 0
         while current is not None:
-            count += 1
+            print(current.val)
             current = current.next
-        return count
 
     def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
         if head is None:
-            return ListNode()
+            return None
+
+        prev = head
+        current = head.next
+
+        result_node = head
+        result_node.next = None
+
+        while current is not None:
+            current_node = result_node
+
+            if prev.val == current.val:
+                current = current.next
+            elif prev.val != current.val:
+                while current_node.next is not None:
+                    current_node = current_node.next
+                current_node.next = ListNode(current.val)
+                prev = current
+        return result_node
+
+
+
 
 
 if __name__ == '__main__':
     a = ListNode(1)
     b = ListNode(1)
     c = ListNode(2)
-    d = ListNode(3)
-    e = ListNode(3)
+    # d = ListNode(3)
+    # e = ListNode(3)
 
     a.next = b
     b.next = c
-    c.next = d
-    d.next = e
+    # c.next = d
+    # d.next = e
 
     solution = Solution()
-    l = solution.getSize(a)
-    print(l)
+    re = solution.deleteDuplicates(a)
+    solution.print(re)
